408e55203647fedc986df0732cbd3b879fd3cf39
Your Name <you@example.com>
2023-10-03 10:28:09 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+import controller.ContactManagementProgram;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new ContactManagementProgram().run();
+    }
+
+}
